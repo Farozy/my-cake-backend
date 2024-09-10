a02d7eea37e03359830ca90974a9ff28a57485ac
farozy <farozy12@gmail.com>
2024-09-10 21:43:08 +0700

    first setup

---
+++ application-core/src/main/java/org/farozy/myCake/MyCakeApplication.java
@@ -0,0 +1,13 @@
+package org.farozy.myCake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCakeApplication.class, args);
+	}
+
+}
